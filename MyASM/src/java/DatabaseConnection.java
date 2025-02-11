934c7a1312fb2336eff49185e410a6c1c50994b2
DangTH2212 <dangthhe176377@fpt.edu.vn>
2025-02-11 22:18:39 +0700

    connect succes
    fix connext erro from database to netbean

--- MyASM/src/java/DatabaseConnection.java
+++ MyASM/src/java/DatabaseConnection.java
@@ -5,15 +5,15 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     public static Connection connect() {
         String url = "jdbc:sqlserver://localhost:1433;databaseName=Logindata;encrypt=true;trustServerCertificate=true";
-        String email = "nguyenvanad@example.com";  // Hoặc user khác nếu có
-        String password = "1"; // Nhập mật khẩu SQL Server
+        String user = "sa";  // Hoặc user khác nếu có
+        String password = "123456"; // Nhập mật khẩu SQL Server
         
         try {
             // Load driver (Không cần thiết với JDBC 4.0 trở lên)
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             
             // Kết nối database
-            Connection conn = DriverManager.getConnection(url, email, password);
+            Connection conn = DriverManager.getConnection(url, user, password);
             System.out.println("✅ Kết nối SQL Server thành công!");
             return conn;
         } catch (ClassNotFoundException | SQLException e) {
